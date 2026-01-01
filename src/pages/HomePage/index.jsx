5829fe7338aed05e4583972226dbdfc8550d5b60
morris menanya <emmiemenz@gmail.com>
2026-01-01 11:18:32 +0100

    update and refactor

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,20 +1,11 @@
-import { useNavigate } from 'react-router-dom'
-import ClassicBtn from '../../compnents/button/ClassicButton'
 import Header from '../../compnents/header/Header'
-import ExxlusiveDeals from './components/deals/ExclusiveDeals'
-import HighlightInfo from './components/highlight/Highlight'
 
 import './style.scss'
-import PopularCards from './components/popular-card/PopularCards'
 import Hero from './components/Hero'
 import Features from './components/Features'
 import HowItWorks from './components/HowItWorks'
 import CallToAction from './components/CallToAction'
-import Footer from './components/Footer'
-import { Toaster } from '../../compnents/UI/toaster'
 const Home = () => {
-  const navigate = useNavigate()
-
   return (
     <div className="-mt-40 md:-mt-40">
       <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-emerald-50">
